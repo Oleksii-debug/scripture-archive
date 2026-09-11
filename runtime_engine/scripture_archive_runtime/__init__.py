f5ad315b80838db74b95a5756b5967877cc18def
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-09-11 21:34:03 +0200

    Export shared evidence provenance helpers

--- runtime_engine/scripture_archive_runtime/__init__.py
+++ runtime_engine/scripture_archive_runtime/__init__.py
@@ -6,6 +6,14 @@ from .package_adapters import adapt_node_for_runtime, derive_answer_dto, load_pa
 from .branching import BranchEngine
 from .content import ContentRepository, validate_canonical_node
 from .evidence import Claim, EvidenceRecord, EvidenceRuntime, Event, PassageRef, Person, Place, Relation
+from .evidence_provenance import (
+    resolve_evidence_witness,
+    resolve_support_witness,
+    validated_claim_witness,
+    validated_declared_witness,
+    validated_relation_witness,
+    visible_relation_passage_ids,
+)
 from .grading import GraderRegistry
 from .mastery import MasteryEngine
 from .models import *
@@ -16,5 +24,7 @@ from .security import CommandEnvelope, ValidationError, validate_command_dto, va
 __all__ = [
     "RuntimeApplication", "ANSWER_CONTRACT_VERSION", "answer_contract_descriptor", "validate_answer_dto", "adapt_node_for_runtime", "derive_answer_dto", "load_package_nodes", "BranchEngine", "ContentRepository", "validate_canonical_node", "GraderRegistry",
     "MasteryEngine", "Scheduler", "PersistenceStore", "EvidenceRuntime", "EvidenceRecord", "PassageRef", "Claim",
-    "Person", "Event", "Place", "Relation", "CommandEnvelope", "ValidationError", "validate_command_dto", "validate_content_import",
+    "Person", "Event", "Place", "Relation", "resolve_evidence_witness", "resolve_support_witness",
+    "validated_declared_witness", "validated_claim_witness", "validated_relation_witness", "visible_relation_passage_ids",
+    "CommandEnvelope", "ValidationError", "validate_command_dto", "validate_content_import",
 ]

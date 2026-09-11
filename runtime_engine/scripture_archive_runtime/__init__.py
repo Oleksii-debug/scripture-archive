4ecc71926be4c2988846dc8d5cfad97bf4847db9
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-09-11 21:40:21 +0200

    R06: expose offline readiness verification contract

--- runtime_engine/scripture_archive_runtime/__init__.py
+++ runtime_engine/scripture_archive_runtime/__init__.py
@@ -9,6 +9,17 @@ from .evidence import Claim, EvidenceRecord, EvidenceRuntime, Event, PassageRef,
 from .grading import GraderRegistry
 from .mastery import MasteryEngine
 from .models import *
+from .offline_readiness import (
+    NETWORK_POLICY_FORBIDDEN,
+    OFFLINE_MANIFEST_SCHEMA,
+    OFFLINE_REPORT_SCHEMA,
+    OfflineDependency,
+    OfflineReadinessError,
+    OfflineReadinessManifest,
+    OfflineReadinessReport,
+    VerifiedOfflineDependency,
+    verify_offline_bundle,
+)
 from .persistence import PersistenceStore
 from .scheduler import Scheduler
 from .security import CommandEnvelope, ValidationError, validate_command_dto, validate_content_import
@@ -17,4 +28,7 @@ __all__ = [
     "RuntimeApplication", "ANSWER_CONTRACT_VERSION", "answer_contract_descriptor", "validate_answer_dto", "adapt_node_for_runtime", "derive_answer_dto", "load_package_nodes", "BranchEngine", "ContentRepository", "validate_canonical_node", "GraderRegistry",
     "MasteryEngine", "Scheduler", "PersistenceStore", "EvidenceRuntime", "EvidenceRecord", "PassageRef", "Claim",
     "Person", "Event", "Place", "Relation", "CommandEnvelope", "ValidationError", "validate_command_dto", "validate_content_import",
+    "NETWORK_POLICY_FORBIDDEN", "OFFLINE_MANIFEST_SCHEMA", "OFFLINE_REPORT_SCHEMA", "OfflineDependency",
+    "OfflineReadinessError", "OfflineReadinessManifest", "OfflineReadinessReport", "VerifiedOfflineDependency",
+    "verify_offline_bundle",
 ]

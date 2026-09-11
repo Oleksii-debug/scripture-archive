264b181fc6ab24a060280b29ca7f6639f774db05
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-09-11 21:00:26 +0200

    R06: expose content pack runtime API

--- runtime_engine/scripture_archive_runtime/__init__.py
+++ runtime_engine/scripture_archive_runtime/__init__.py
@@ -5,6 +5,14 @@ from .answer_contracts import ANSWER_CONTRACT_VERSION, answer_contract_descripto
 from .package_adapters import adapt_node_for_runtime, derive_answer_dto, load_package_nodes
 from .branching import BranchEngine
 from .content import ContentRepository, validate_canonical_node
+from .content_packs import (
+    CONTENT_PACK_SCHEMA,
+    CONTENT_SCHEMA_VERSION,
+    ContentPackInspection,
+    ContentPackManifest,
+    ContentPackStore,
+    inspect_content_pack,
+)
 from .evidence import Claim, EvidenceRecord, EvidenceRuntime, Event, PassageRef, Person, Place, Relation
 from .grading import GraderRegistry
 from .mastery import MasteryEngine
@@ -17,4 +25,5 @@ __all__ = [
     "RuntimeApplication", "ANSWER_CONTRACT_VERSION", "answer_contract_descriptor", "validate_answer_dto", "adapt_node_for_runtime", "derive_answer_dto", "load_package_nodes", "BranchEngine", "ContentRepository", "validate_canonical_node", "GraderRegistry",
     "MasteryEngine", "Scheduler", "PersistenceStore", "EvidenceRuntime", "EvidenceRecord", "PassageRef", "Claim",
     "Person", "Event", "Place", "Relation", "CommandEnvelope", "ValidationError", "validate_command_dto", "validate_content_import",
+    "CONTENT_PACK_SCHEMA", "CONTENT_SCHEMA_VERSION", "ContentPackManifest", "ContentPackInspection", "ContentPackStore", "inspect_content_pack",
 ]

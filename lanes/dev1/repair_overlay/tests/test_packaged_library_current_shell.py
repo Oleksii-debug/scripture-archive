e1454365b68c2ba8f9c3de6c7a6f6acee1a68793
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-09-12 14:41:41 +0200

    R06: qualify Witness Matrix in current packaged shell

--- lanes/dev1/repair_overlay/tests/test_packaged_library_current_shell.py
+++ lanes/dev1/repair_overlay/tests/test_packaged_library_current_shell.py
@@ -37,6 +37,7 @@ class PackagedLibraryCurrentShellTest(unittest.TestCase):
             "./library-shell-compat.js",
             "./daily-case-ui.js",
             "./content-pack-manager.js",
+            "./witness-matrix-ui.js",
         }
         imports = set(re.findall(r"void import\('([^']+)'\)", self.transport))
         self.assertEqual(imports, expected_loaders)
@@ -109,6 +110,7 @@ class PackagedLibraryCurrentShellTest(unittest.TestCase):
             ROOT / "library-shell-compat.js",
             ROOT / "daily-case-ui.js",
             ROOT / "content-pack-manager.js",
+            ROOT / "witness-matrix-ui.js",
         ):
             completed = subprocess.run(
                 ["node", "--check", str(path)],
